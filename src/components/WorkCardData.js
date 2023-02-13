7de90d016494997b62d2794212fc41504a8dabd8
xin <lx1326607292@gmail.com>
2023-02-13 15:23:37 -0500

    updated

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,28 +1,27 @@
-import pro1 from "../assets/projects.png"
+import pro1 from "../assets/personalWeb.png"
 import pro2 from "../assets/projects.png"
 import pro3 from "../assets/projects.png"
-import NoAvailiable from "./NoAvailiable";
 const ProjectCardData = [
     {
         imgsrc:pro1,
         title:"Personal web design",
         text:"Personal web that contains XIN's infomations and projects.",
-        view:"https://github.com/lusanjj/xinliu",
+        view:"https://liuxin02.com/",
         source:"https://github.com/lusanjj/xinliu",
     },
     {
         imgsrc:pro2,
-        title:"xxx project",
-        text:"xxx simutualtor",
-        view:"https://github.com/lusanjj/xinliu",
-        source:"https://github.com/lusanjj/xinliu",
+        title:"xxx project xxx",
+        text:"xxx description xxx",
+        view:"https://github.com/lusanjj/xinliu/blob/main/unavailable.md",
+        source:"https://github.com/lusanjj/xinliu/blob/main/unavailable.md",
     },
     {
         imgsrc:pro3,
-        title:"xxx project",
-        text:"xxx simutualtor",
-        view:"https://github.com/lusanjj/xinliu",
-        source:"https://github.com/lusanjj/xinliu",
+        title:"xxx project xxx",
+        text:"xxx description xxx",
+        view:"https://github.com/lusanjj/xinliu/blob/main/unavailable.md",
+        source:"https://github.com/lusanjj/xinliu/blob/main/unavailable.md",
     },
 ];
 export default ProjectCardData;
