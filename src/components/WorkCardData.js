a64f596c18427dbccdc3bf5976fa451eae37adb9
xin <lx1326607292@gmail.com>
2023-02-13 02:41:40 -0500

    updated

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,27 +1,28 @@
 import pro1 from "../assets/projects.png"
 import pro2 from "../assets/projects.png"
 import pro3 from "../assets/projects.png"
+import NoAvailiable from "./NoAvailiable";
 const ProjectCardData = [
     {
         imgsrc:pro1,
-        title:"Commerial web design",
-        text:"Online shopping simutualtor",
-        view:"https://youtu.be/tKwPzva-tOc",
-        source:"",
+        title:"Personal web design",
+        text:"Personal web that contains XIN's infomations and projects.",
+        view:"https://github.com/lusanjj/xinliu",
+        source:"https://github.com/lusanjj/xinliu",
     },
     {
         imgsrc:pro2,
         title:"xxx project",
         text:"xxx simutualtor",
-        view:"https://youtu.be/tKwPzva-tOc",
-        source:"",
+        view:"https://github.com/lusanjj/xinliu",
+        source:"https://github.com/lusanjj/xinliu",
     },
     {
         imgsrc:pro3,
         title:"xxx project",
         text:"xxx simutualtor",
-        view:"https://youtu.be/tKwPzva-tOc",
-        source:"",
+        view:"https://github.com/lusanjj/xinliu",
+        source:"https://github.com/lusanjj/xinliu",
     },
 ];
 export default ProjectCardData;
